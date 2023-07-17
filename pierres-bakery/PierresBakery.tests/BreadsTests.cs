77991b2fb8004744d5cfa629bacf9767b79ba019
ellehailu <hailu.elle@gmail.com>
2023-07-16 20:15:28 -0700

    getter method test passed

--- pierres-bakery/PierresBakery.tests/BreadsTests.cs
+++ pierres-bakery/PierresBakery.tests/BreadsTests.cs
@@ -16,6 +16,14 @@ namespace PierresBakery.Tests
             Bread newBread = new Bread(breadAmount);
             Assert.AreEqual(typeof(Bread), newBread.GetType());
         }
+
+        [TestMethod]
+        public void GetBreadAmount_ReturnBreadAmount_Int(){
+            int breadAmount = 3;
+            Bread newBread = new Bread(breadAmount);
+            int expectedAmount = newBread.BreadAmount;
+            Assert.AreEqual(breadAmount, expectedAmount);
+        }
     }
 }
 
