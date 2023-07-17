0d2176a97fa7d52006f1757e989c43bbf843e159
ellehailu <hailu.elle@gmail.com>
2023-07-16 22:08:24 -0700

    add a method to allow the user to add to order

--- pierres-bakery/PierresBakery/program.cs
+++ pierres-bakery/PierresBakery/program.cs
@@ -13,7 +13,12 @@ namespace PierresBakery
             Console.WriteLine("Bread: $5/Loaf --Buy two and get your third one free \n every third loaf of bread is free!");
             Console.WriteLine("Pastries: $2/Pastry --Buy three and get your fourth one free \n every fourth pastry is free!");
         }
-
+        static void UpdateOrder(Bread breadAmount, Pastries pastriesAmount){
+            Console.WriteLine("How many loaves of bread would you like to buy?");
+            string breadInput = Console.ReadLine();
+            Console.WriteLine("How many pastries would you like to buy?");
+            string pastriesInput = Console.ReadLine();
+        }
         
     }
 }
