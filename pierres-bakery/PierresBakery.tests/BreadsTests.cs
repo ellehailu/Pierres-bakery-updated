2d47eae2560e9576023f2987aad2ff1b0829ba0a
ellehailu <hailu.elle@gmail.com>
2023-07-16 22:02:42 -0700

    add a main method with a welcome message

--- pierres-bakery/PierresBakery.tests/BreadsTests.cs
+++ pierres-bakery/PierresBakery.tests/BreadsTests.cs
@@ -1,49 +1,49 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using System;
-// using System.Collections.Generic;
-// using PierresBakery.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using PierresBakery.Models;
 
-// namespace PierresBakery.Tests
-// {
-//     [TestClass]
+namespace PierresBakery.Tests
+{
+    [TestClass]
 
-//     public class BreadTests
-//     {
-//         [TestMethod]
-//         public void BreadConstructor_CreatesBreadObject_BreadObject()
-//         {
-//             int breadAmount = 3;
-//             Bread newBread = new Bread(breadAmount);
-//             Assert.AreEqual(typeof(Bread), newBread.GetType());
-//         }
+    public class BreadTests
+    {
+        [TestMethod]
+        public void BreadConstructor_CreatesBreadObject_BreadObject()
+        {
+            int breadAmount = 3;
+            Bread newBread = new Bread(breadAmount);
+            Assert.AreEqual(typeof(Bread), newBread.GetType());
+        }
 
-//         [TestMethod]
-//         public void GetBreadAmount_ReturnBreadAmount_Int(){
-//             int breadAmount = 3;
-//             Bread newBread = new Bread(breadAmount);
-//             int expectedAmount = newBread.BreadAmount;
-//             Assert.AreEqual(breadAmount, expectedAmount);
-//         }
+        [TestMethod]
+        public void GetBreadAmount_ReturnBreadAmount_Int(){
+            int breadAmount = 3;
+            Bread newBread = new Bread(breadAmount);
+            int expectedAmount = newBread.BreadAmount;
+            Assert.AreEqual(breadAmount, expectedAmount);
+        }
 
-//         [TestMethod]
-//         public void SetBreadAmount_SetAmountOfBread_Void(){
-//             int breadAmount = 3;
-//             Bread newBread = new Bread(breadAmount);
-//             int updatedAmount = 7;
-//             newBread.BreadAmount = updatedAmount;
-//             Assert.AreEqual(updatedAmount, newBread.BreadAmount);
-//         }
+        [TestMethod]
+        public void SetBreadAmount_SetAmountOfBread_Void(){
+            int breadAmount = 3;
+            Bread newBread = new Bread(breadAmount);
+            int updatedAmount = 7;
+            newBread.BreadAmount = updatedAmount;
+            Assert.AreEqual(updatedAmount, newBread.BreadAmount);
+        }
 
-//         [TestMethod]
-//         public void CalculateBreadPrice_CalculatePriceOfBread_Int()
-//         {
-//             int breadAmount = 3;
-//             Bread newBread = new Bread(breadAmount);
-//             int price = newBread.CalculatePrice();
-//             Assert.AreEqual(9, price);
-//         }
-//     }
-// }
+        [TestMethod]
+        public void CalculateBreadPrice_CalculatePriceOfBread_Int()
+        {
+            int breadAmount = 3;
+            Bread newBread = new Bread(breadAmount);
+            int price = newBread.CalculatePrice();
+            Assert.AreEqual(9, price);
+        }
+    }
+}
 
 
 
--- pierres-bakery/PierresBakery/program.cs
+++ pierres-bakery/PierresBakery/program.cs
@@ -1,17 +1,20 @@
+using System;
+using PierresBakery.Models;
+
 namespace PierresBakery
 {
     class Program
     {
         static void Main()
         {
-            // ui logic 
-            // welcome message 
-            // list prices for bread and pastries 
-            // ask the user whether they want to buy pastries or bread
-            // ask for quantity of items
-            // ask if they want to add any thing else to their order
-            //
+
+            Console.WriteLine("Welcome to Pierre's Bakery");
+            Console.WriteLine("Our prices are listed below: ");
+            Console.WriteLine("Bread: $5/Loaf --Buy two and get your third one free \n every third loaf of bread is free!");
+            Console.WriteLine("Pastries: $2/Pastry --Buy three and get your fourth one free \n every fourth pastry is free!");
         }
+
+        
     }
 }
 
