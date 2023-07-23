9ec516c586eb3eee36d0ae996e58e9fc8720d8dc
ellehailu <hailu.elle@gmail.com>
2023-07-23 14:40:53 -0700

    added try catch block to validate user input

--- PierresBakery/program.cs
+++ PierresBakery/program.cs
@@ -14,12 +14,22 @@ namespace PierresBakery
             Console.WriteLine("Pastries: $2/Pastry --Buy three and get your fourth one free \n every fourth pastry is free!");
             UpdateOrder();
         }
-        static void UpdateOrder(){
-            Console.WriteLine("How many loaves of bread would you like to buy?");
-            int breadInput = int.Parse(Console.ReadLine());
-            Console.WriteLine("How many pastries would you like to buy?");
-            int pastriesInput = int.Parse(Console.ReadLine());
+        static void UpdateOrder()
+        {
 
+            try
+            {
+                Console.WriteLine("How many loaves of bread would you like to buy?");
+                int breadInput = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("How many pastries would you like to buy?");
+                int pastriesInput = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("please enter a valid input")
+                return;
+            }
             Pastries pastriesOrder = new Pastries(pastriesInput);
             Bread breadOrder = new Bread(breadInput);
             ConfirmOrder(breadOrder, pastriesOrder);
@@ -27,28 +37,33 @@ namespace PierresBakery
             //maybe add try/catch to filter user input that is not integers
         }
 
-        static void ConfirmOrder(Bread breadAmount, Pastries pastriesAmount){
+        static void ConfirmOrder(Bread breadAmount, Pastries pastriesAmount)
+        {
             Console.WriteLine($"Please confirm your order: you would like to purchase {breadAmount.BreadAmount} loaves of bread and {pastriesAmount.PastriesAmount} pastry/pastries?");
             Console.WriteLine("Is this order correct? please type in 'yes' to continue or 'no' to re-enter the correct amounts");
             string orderConfirmation = Console.ReadLine();
-            if (orderConfirmation == "yes" || orderConfirmation == "YES" || orderConfirmation == "Yes"){
+            if (orderConfirmation == "yes" || orderConfirmation == "YES" || orderConfirmation == "Yes")
+            {
                 Console.WriteLine("Thank you for confirming your order!");
                 CalculateTotalPrice(breadAmount, pastriesAmount);
             }
-            else if(orderConfirmation == "no" || orderConfirmation == "NO" || orderConfirmation == "No"){
+            else if (orderConfirmation == "no" || orderConfirmation == "NO" || orderConfirmation == "No")
+            {
                 UpdateOrder();
             }
-            else{
+            else
+            {
                 Console.WriteLine("please enter a valid response");
                 ConfirmOrder(breadAmount, pastriesAmount);
             }
         }
-        static void CalculateTotalPrice(Bread breadAmount, Pastries pastriesAmount){
+        static void CalculateTotalPrice(Bread breadAmount, Pastries pastriesAmount)
+        {
             int totalPrice = breadAmount.CalculateBreadPrice() + pastriesAmount.CalculatePastryPrice();
             Console.WriteLine($"The total cost for your order is ${totalPrice}");
             Console.WriteLine("Thanks for visiting Pierre's Bakery!");
         }
-        
+
     }
 }
 
